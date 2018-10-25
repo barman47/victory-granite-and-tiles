43f9951053542efc5157242217d3b8af74e6b0e0
barman47 <nomsouzoanya@yahoo.co.uk>
2018-10-25 22:12:46 +0100

    Added Mobile Navigation

--- public/js/index.js
+++ public/js/index.js
@@ -6,9 +6,32 @@ $(document).ready(function () {
     $('.scrollspy').scrollSpy();
     $('.modal').modal();
     $('.parallax').parallax();
+    $('.sidenav').sidenav();
 
     var form = document.messageForm;
     var ajaxLinks = document.querySelectorAll('.ajaxLink');
+
+    $('#top').on('click', function (event) {
+        if (this.hash !== '') {
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+
+    window.onscroll = function() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            document.getElementById('top').style.display = 'block';
+        } else {
+            document.getElementById('top').style.display = 'none';	
+        }
+    };
 
     function fetchContent (event) {
         var url = event.target.dataset.href;
