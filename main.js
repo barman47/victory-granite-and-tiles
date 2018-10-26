24a7485a776dab21324ece01b88a837de725fef6
barman47 <nomsouzoanya@yahoo.co.uk>
2018-10-26 17:54:26 +0100

    Fixed Messaging

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const favicon = require('express-favicon');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
 const app = express();
 
 const port = process.env.PORT || 3000;
@@ -9,6 +11,8 @@ const publicPath = path.join(__dirname, 'public');
 const faviconPath = path.join(publicPath, 'img', 'favicon.png');
 
 app.use(express.static(publicPath));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 app.use(favicon(faviconPath));
 app.engine('.hbs', exphbs({
     extname: '.hbs',
@@ -22,6 +26,49 @@ app.get('/', (req, res) => {
         title: 'Victory Granite and Tiles',
         style: 'index.css',
         script: 'index.js'
+    });
+});
+
+app.post('/email', (req, res) => {
+    var email = 'No Email Provided';
+    const message = `
+    <h3>YOU HAVE A NEW CUSTOMER REQUEST</h3>
+    <br>
+    <h3>Customer Details</h3>
+    <P>Customer Name: ${req.body.name}</P>
+    <P>Customer Phone: ${req.body.phone}</P>
+    <P>Customer Email: ${req.body.email || email}</P>
+    <h2>Message Body</h2>
+    <p>${req.body.message}</p>
+    `;
+
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'victorycustomerorder@gmail.com',
+          pass: 'VICtory@2018',
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+      });
+
+       // setup email data with unicode symbols
+    let mailOptions = {
+        from: `Victory Customer Handler`, // sender address
+        to: 'victorygranitesandtiles@gmail.com', // list of receivers
+        subject: 'HELLO, I REQUIRE YOUR SERVICES', // Subject line
+        html: message // html body
+    };
+      
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            return console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.status(200).end();
+        }
     });
 });
 
--- public/js/index.js
+++ public/js/index.js
@@ -9,7 +9,83 @@ $(document).ready(function () {
     $('.sidenav').sidenav();
 
     var form = document.messageForm;
+    var name = form.customerName;
+    var email = form.email;
+    var phone = form.phone;
+    var message = form.message;
     var ajaxLinks = document.querySelectorAll('.ajaxLink');
+
+    var phoneRegExp = /^\d{11}$/;
+    var emailRegExp =  /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
+
+    function isEmpty (element) {
+        if (element.value === '' || element.value.trim() === '') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function checkInputs () {
+        if (isEmpty(name)) {
+            M.toast({ html: 'Please enter your name' });
+            name.focus();
+            return false;
+        } else if (isEmpty(phone)) {
+            M.toast({ html: 'Please provide your phone number' });
+            phone.classList.add('invalid');
+            phone.focus();
+            return false;
+        } else if (isEmpty(message)) {
+            M.toast({ html: 'Please fill out the product description' });
+            message.classList.add('invalid');
+            message.focus();
+            return false;
+        }
+        return true
+    }
+
+    function sendEmail () {
+        $('#messageButton').html('SENDING MESSAGE . . .');
+        form.disabled = true;
+        let payload = {
+            name: $('#customerName').val(),
+            email: $('#email').val(),
+            phone: $('#phone').val(),
+            message: $('#message').val()
+        };
+
+        const url = '/email';
+        
+        $.ajax({
+            type: 'POST',
+            url,
+            data: payload
+        }).done (function () {
+            M.toast({
+                html: 'Message Sent. You will be contacted Shortly.',
+                classes: 'rounded',
+            });
+            $('#messageButton').html('SEND MESSAGE');
+            form.reset();
+        }).fail(function (jqXHR, status) {
+            M.toast({
+                html: 'Message not Sent. Please make sure you have an active internet connection.',
+                classes: 'rounded'
+            });
+            $('#messageButton').html('SEND MESSAGE');
+            form.disabled = false;
+        });   
+    }
+
+    function sendMessage (event) {
+        event.preventDefault();
+        if (isEmpty(name) || isEmpty(phone) || isEmpty(message)) {
+            checkInputs();
+        } else {
+            sendEmail();
+        }
+    }
 
     $('#top').on('click', function (event) {
         if (this.hash !== '') {
@@ -43,11 +119,28 @@ $(document).ready(function () {
         });
     }
 
+    email.addEventListener('keyup', function () {
+        if (emailRegExp.test(email.value)) {
+            email.classList.add('valid');
+            email.classList.remove('invalid');
+        } else {
+            email.classList.add('invalid');
+            email.classList.remove('valid');
+        }
+    });
+    phone.addEventListener('keyup', function () {
+        if (phoneRegExp.test(phone.value)) {
+            phone.classList.add('valid');
+            phone.classList.remove('invalid');
+        } else {
+            phone.classList.add('invalid');
+            phone.classList.remove('valid');
+        }
+    });
+
     ajaxLinks.forEach(function(link) {
         link.addEventListener('click', fetchContent);
     });
 
-    form.addEventListener('submit', function (event) {
-        event.preventDefault();
-    });
+    form.addEventListener('submit', sendMessage);
 });
